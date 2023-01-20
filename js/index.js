b6f4c34beb48088fcb8811248eea579d359b66f5
Samuel Herrera <sherrera-2021080@kinal.edu.gt>
2023-01-20 08:59:26 -0600

    Primer commit v1 webpage1

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+function sumar(){
+
+    let numA = document.getElementById('valorA').value;
+    let numB = document.getElementById('valorB').value;
+
+    let resultado = parseInt(numA) + parseInt (numB);
+
+    console.log(resultado);
+}
